0e35ecd5a5a7d2fb556af5afc23150a8461f56a6
vishal1202verma <vishalvermao.pcm121@gmail.com>
2024-01-10 23:09:23 +0530

    commited

---
+++ Week7_string/String-2Assignment/Answer6.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main (){
+// this is not the assignment question
+// it is only for github strick
+int a, b;
+cout<<"enter the integer a :";
+cin>>a;
+cout<<"enter the integer b :";
+cin>>b;
+int sum = a+b;
+cout<<"the sum of the integer is :"<<sum;
+
+
+}
