2c35b76c3237c5f49b105523cd51fff7c185ed21
vishal1202verma <vishalvermao.pcm121@gmail.com>
2024-02-07 23:37:56 +0530

    comitted

---
+++ Week9_sortingAssignment/Bubble Sort-1/Answer-1.cpp
@@ -0,0 +1,16 @@
+// What will the following array look like after one iteration of bubble sort [1,6,2,5,4,3].
+// [1,3,2,4,5,6]
+// [1,2,3,4,5,6]
+// [1,2,5,4,3,6]
+// [1,2,4,5,3,6]
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+using namespace std;
+int main (){
+
+cout<<"vishal verma ji ";
+
+
+}
