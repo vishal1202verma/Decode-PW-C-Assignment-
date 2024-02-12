81c3a3507c684c5c2db87f847b415d59b171e162
vishal1202verma <vishalvermao.pcm121@gmail.com>
2024-02-12 23:18:21 +0530

    BInary Search-1 question-3 Assignment commited

--- Week10_searchingAssgn/BinarySearch-1/Answer3.cpp
+++ Week10_searchingAssgn/BinarySearch-1/Answer3.cpp
@@ -1,28 +1,48 @@
 // Given a sorted binary array, efficiently count the total number of 1’s in it.
 // Input 1 : a = [0,0,0,0,1,1]
 #include<iostream>
+#include<vector>
 using namespace std;
-int main(){
-int arr[] = {0,0,0,0,1,1};
-int n = sizeof(arr)/sizeof(arr[0]);
-int lo =0;
-int hi = n-1;
-int count =0;
-int target = 1;
-while(lo<=hi){
-    int mid = lo + (hi-lo)/2;
-    if(arr[mid] == target){
-        count++;
-    }
-    else if (arr[mid]<target){
-        lo = mid +1;
-    }
-    else{
-        hi = mid-1;
-    }
-} 
+// int main(){
+// int arr[] = {0,0,0,0,1,1};
+// int n = sizeof(arr)/sizeof(arr[0]);
+// int lo =0;
+// int hi = n-1;
+// int count =0;
+// int target = 1;
+// while(lo<=hi){
+//     int mid = lo + (hi-lo)/2;
+//     if(arr[mid] == target){
+//         count++;
+//     }
+//     else if (arr[mid]<target){
+//         lo = mid +1;
+//     }
+//     else{
+//         hi = mid-1;
+//     }
+// } 
 
-cout<<count;
+// cout<<count;
 
+int firstOccurrence(vector<int>&a , int n , int tgt){
+ int low = 0 , high = n - 1;
+ int ans = -1;
+ while(low <= high){
+ int mid = low + (high - low)/2;
+ if(a[mid] == tgt){
+ ans = mid;
+ high = mid - 1;
+ }
+ else low = mid + 1;
+ }
+ return low;
+}
+int main() {
+ int n;
+ cin>>n;
+ vector<int>a(n);
+ for(int i=0;i<n;i++)cin>>a[i];
+ cout<<n - firstOccurrence(a , n , 1);
 
 }
