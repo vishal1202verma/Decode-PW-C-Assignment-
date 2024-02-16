0fb63d8e492d19f3c629d34b1fc6a75032aaaa04
vishal1202verma <vishalvermao.pcm121@gmail.com>
2024-02-16 19:56:25 +0530

    Recursion-2 is commited

---
+++ Week11-Recursion/AssignRecursion-2/AnswerOne.cpp
@@ -0,0 +1,18 @@
+// Print all the elements of an array in reverse order.
+#include<iostream>
+using namespace std;
+void PrintDisplay(int arr[], int n, int idx){
+  // base case 
+  if(n == idx) return;
+  cout<<arr[idx]<<" ";
+  PrintDisplay(arr, n,idx-1 );
+  
+}
+
+
+int main (){
+int arr [] = {1,2,3,4,5,6};
+int n  = sizeof(arr)/sizeof(arr[0]);
+PrintDisplay(arr, 0, n);
+return 0;
+}
